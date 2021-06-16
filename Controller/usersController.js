0445144bfe315044c4cf5bb23cb0fc02c3217187
EmilMirzeyev <85557943+EmilMirzeyev@users.noreply.github.com>
2021-06-16 10:36:08 +0400

    Update usersController.js

--- Controller/usersController.js
+++ Controller/usersController.js
@@ -4,7 +4,7 @@ const response = require("./../response");
 const db = require("./../settings/db");
 
 exports.users = (req, res) => {
-	db.query("SELECT * FROM users ", (error, rows, fields) => {
+	db.query("SELECT * FROM table_name ", (error, rows, fields) => {
 		if (error) {
 			console.log(error);
 		} else {
